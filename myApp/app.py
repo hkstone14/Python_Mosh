6adf357919414794276adb5fb7a1403accbeef49
harikrushna <hp52@njit.edu>
2021-07-29 19:13:12 -0400

    Update: Minor update

--- myApp/app.py
+++ myApp/app.py
@@ -1,9 +1,12 @@
+import json
+from pathlib import Path
+import sqlite3
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import smtplib
 
 
-#email module added with message body
+#email module added with
 message = MIMEMultipart()
 message["from"] = "Harikrushna Patel"
 message["to"] = "abc123@gmail.com"
@@ -17,40 +20,25 @@ with smtplib.SMTP(host="smtp.gmail.com", port=587) as smtp:
     print("Sent...")
 
 
+# SQLite3 db insert and get
+data = Path("movies.json").read_text()
+movies = json.loads(data)
+
+# Inserting into the Database
+with sqlite3.connect("db.sqlite3") as conn:
+    command = "INSERT INTO Movies VALUES(?, ?, ?)"
+    for movie in movies:
+        conn.execute(command, tuple(movie.values()))
+    conn.commit()
 
 
-
-
-
-
-
-
-
-
-
-# import json
-# from pathlib import Path
-# import sqlite3
-#
-#
-# data = Path("movies.json").read_text()
-# movies = json.loads(data)
-#
-# # Inserting into the Database
-# with sqlite3.connect("db.sqlite3") as conn:
-#     command = "INSERT INTO Movies VALUES(?, ?, ?)"
-#     for movie in movies:
-#         conn.execute(command, tuple(movie.values()))
-#     conn.commit()
-#
-#
-# # Retrieve from Database
-# with sqlite3.connect("db.sqlite3") as conn:
-#     command = "SELECT * from Movies"
-#     cursor = conn.execute(command)
-#     # for row in cursor:
-#     #     print(row)
-#     movies_fetch = cursor.fetchall()
-#     print(movies_fetch)
+# Retrieve from Database
+with sqlite3.connect("db.sqlite3") as conn:
+    command = "SELECT * from Movies"
+    cursor = conn.execute(command)
+    # for row in cursor:
+    #     print(row)
+    movies_fetch = cursor.fetchall()
+    print(movies_fetch)
 
 
